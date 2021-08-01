6404e6bbdad8706234f944b73d9fe2f8a9d22b06
Danatey <74828367+Danatey@users.noreply.github.com>
2021-08-01 10:09:35 +0300

    Updates

--- static/js/main.db5dd303.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-04-hooks-feedback"]=this["webpackJsonpgoit-react-hw-04-hooks-feedback"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var o=c(1),s=c.n(o),n=c(3),r=c.n(n),a=c(0);var i=function(){return Object(a.jsx)("div",{className:"App"})};r.a.render(Object(a.jsx)(s.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.db5dd303.chunk.js.map
